7e039575513caf218f7c6bec605d6c00d176311f
joseph.lauth <joseph.lauth@CPX-5UCKMCA0IMV.dir.svc.accenture.com>
2020-03-10 13:26:36 -0500

    first commit

---
+++ Python3_6_5/tensorFlowScript/TensorFlowExample.py
@@ -0,0 +1,21 @@
+'''
+Created on Mar 5, 2020
+
+@author: joseph.lauth
+'''
+import tensorflow as tf
+import tensorflow_hub as hub
+
+# Create graph and finalize (finalizing optional but recommended).
+g = tf.Graph()
+with g.as_default():
+  # We will be feeding 1D tensors of text into the graph.
+  text_input = tf.placeholder(dtype=tf.string, shape=[None])
+  embed = hub.Module("https://tfhub.dev/google/universal-sentence-encoder/2")
+  embedded_text = embed(text_input)
+  init_op = tf.group([tf.global_variables_initializer(), tf.tables_initializer()])
+g.finalize()
+
+# Create session and initialize.
+session = tf.Session(graph=g)
+session.run(init_op)
---
+++ Python3_6_5/tensorFlowScript/troubleShootScript.py
@@ -0,0 +1,17 @@
+'''
+Created on Mar 6, 2020
+
+@author: joseph.lauth
+'''
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print("tensorFlow version installed on this machine: " + tf.__version__)
